e4ac7d5deee09b969aa294cd7dc3305d0a3eb8e4
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-22 18:17:16 +0530

    add auth router

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from src.routers import user_router, competition_router, entry_router
+from src.routers import auth_router
+from src.routers import competition_router
+from src.routers import entry_router
+from src.routers import user_router
 
 src = FastAPI()
-
 
 src.include_router(user_router.user_router, prefix='/user',tags=["user"])
 src.include_router(competition_router.competition_router, prefix='/competition', tags=["competition"])
 src.include_router(entry_router.entry_router, prefix='/entry', tags=["entry"])
+src.include_router(auth_router.auth_router, prefix='/auth', tags=["Authentication"])
 
 src.mount("/static", StaticFiles(directory="static"), name="static")

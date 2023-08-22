15ad1035031a6ab6e77eeb21de4c93b786b56b66
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-22 16:18:49 +0530

    add appropriate tag for router

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from fastapi.staticfiles import StaticFiles
 from src.routers import user_router, competition_router, entry_router
 
 src = FastAPI()
-src.mount("/static", StaticFiles(directory="static"), name="static")
 
-src.include_router(user_router.user_router, prefix='/user')
-src.include_router(competition_router.competition_router, prefix='/competition')
-src.include_router(entry_router.entry_router, prefix='/entry')
+
+src.include_router(user_router.user_router, prefix='/user',tags=["user"])
+src.include_router(competition_router.competition_router, prefix='/competition', tags=["competition"])
+src.include_router(entry_router.entry_router, prefix='/entry', tags=["entry"])
+
+src.mount("/static", StaticFiles(directory="static"), name="static")

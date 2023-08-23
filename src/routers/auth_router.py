d8fb34f0af1d038e359c4bb3c4689ca8139a281a
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-23 12:10:47 +0530

    add post route for register user

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.services.auth_crud import registration
+from src.config import SessionLocal
+from src.schemas.auth import AuthSchema
 
 auth_router = APIRouter()
+
+def get_db():
+   db = SessionLocal()
+   try:
+       yield db
+   finally:
+       db.close()
+
+@auth_router.post('/register')
+async def register_user(auth:AuthSchema, db:Session = Depends(get_db)):
+    new_auth_data = registration(auth,db)
+    return {"auth":new_auth_data, "message":"new user created"}

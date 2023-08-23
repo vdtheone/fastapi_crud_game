2faa546372e751134649ac7de0266c2f8e9dcef6
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-23 12:08:58 +0530

    change one column name

--- src/models/auth.py
+++ src/models/auth.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, Date, DateTime, Integer, String
 from src.config import Base
 
 
-class auth(Base):
-    __tablename__ = "auhts"
+class Auth(Base):
+    __tablename__ = "auths"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    username: Column(String, unique=True,index=True)
+    username = Column(String, unique=True,index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     gender = Column(String)
     age = Column(Integer)
-    birthdate = Column(DateTime)
+    date_of_birth = Column(Date)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime)
     updated_at = Column(DateTime)

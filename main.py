97b17236ec99ca374e26f7ea86cd0759478d9589
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-25 11:36:48 +0530

    capitalized first letter of router target

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from src.routers import user_router
 src = FastAPI()
 
 src.include_router(auth_router.auth_router, prefix='/auth', tags=["Authentication"])
-src.include_router(user_router.user_router, prefix='/user',tags=["user"])
-src.include_router(competition_router.competition_router, prefix='/competition', tags=["competition"])
-src.include_router(entry_router.entry_router, prefix='/entry', tags=["entry"])
+src.include_router(user_router.user_router, prefix='/user',tags=["User"])
+src.include_router(competition_router.competition_router, prefix='/competition', tags=["Competition"])
+src.include_router(entry_router.entry_router, prefix='/entry', tags=["Entry"])
 
 src.mount("/static", StaticFiles(directory="static"), name="static")

0481c92854c3ee8d99196d2ed6c906bf74de3e2e
vdtheone <vishalprajapati22052001@gmail.com>
2023-08-29 15:39:52 +0530

    complete all entry route with decorator and error handling

--- src/routers/entry_router.py
+++ src/routers/entry_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from src.config import SessionLocal
 from src.services.entry_crud import (
@@ -9,8 +9,6 @@ from src.services.entry_crud import (
     get_entry_by_id,
     update_entry,
 )
-from src.models.user import User
-from src.models.competition import Competition
 from src.schemas.entry import EntryCreateSchema, EntryUpdateSchema
 
 entry_router = APIRouter()
@@ -25,52 +23,36 @@ def get_db():
 
 
 @entry_router.get("/")
-async def get(db: Session = Depends(get_db)):
-    all_entries = get_all_entries(db, 0, 100)
+async def get(request:Request, db: Session = Depends(get_db)):
+    all_entries = get_all_entries(request, db, 0, 100)
     return all_entries
 
 
 @entry_router.post("/create/")
-async def create(entry: EntryCreateSchema, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == entry.user_id).first()
-    competition = (
-        db.query(Competition).filter(Competition.id == entry.competition_id).first()
-    )
-
-    if user and competition:
-        new_entry = create_entry(user.id, competition.id, db)
-        return {"entry": new_entry, "message": "new entry created"}
-    else:
-        return {"message": "Error"}
+async def create(request:Request, entry: EntryCreateSchema, db: Session = Depends(get_db)):
+    new_entry = create_entry(request, entry, db)
+    return new_entry
 
 
 @entry_router.get("/entry/{entry_id}")
-async def get_entry(entry_id: int, db: Session = Depends(get_db)):
-    one_entry = get_entry_by_id(db, entry_id)
+async def get_entry(request:Request, entry_id: int, db: Session = Depends(get_db)):
+    one_entry = get_entry_by_id(request, db, entry_id)
     return one_entry
 
 
 @entry_router.put("/update/{id}")
-async def update(id: int, entry: EntryUpdateSchema, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == entry.user_id).first()
-    competition = (
-        db.query(Competition).filter(Competition.id == entry.competition_id).first()
-    )
-
-    if user and competition:
-        new_entry = update_entry(id, user.id, competition.id, db)
-        return new_entry
-    else:
-        return {"message": "Error"}
+async def update(request:Request, id: int, entry: EntryUpdateSchema, db: Session = Depends(get_db)):
+    new_entry = update_entry(request, id, entry, db)
+    return new_entry
 
 
 @entry_router.delete("/delete/{id}")
-async def delete(id: int, db: Session = Depends(get_db)):
-    message = delete_entry(id, db)
-    return {"message": message}
+async def delete(request:Request, id: int, db: Session = Depends(get_db)):
+    message = delete_entry(request, id, db)
+    return message
 
 
 @entry_router.delete("/delete_all/")
-async def delete_all(db: Session = Depends(get_db)):
-    delete_all_entry(db)
+async def delete_all(request:Request, db: Session = Depends(get_db)):
+    delete_all_entry(request, db)
     return {"message": "all entries deleted"}
--- src/services/entry_crud.py
+++ src/services/entry_crud.py
@@ -1,45 +1,77 @@
-from src.schemas.entry import EntrySchema
+from fastapi import HTTPException, Request
 from sqlalchemy.orm import Session
+from src.models.competition import Competition
+from src.models.user import User
+from src.schemas.entry import EntryCreateSchema, EntryUpdateSchema
 from src.models.entry import Entry
+from src.utils.return_jwt_token import access_token_required
+from jose import jwt
+import os
 
 
-def get_all_entries(db: Session, skip: int = 0, limit: int = 100):
+SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+
+
+@access_token_required
+def get_all_entries(request:Request, db: Session, skip: int = 0, limit: int = 100):
     return db.query(Entry).offset(skip).limit(limit).all()
 
-
-def create_entry(user_id: int, competition_id: int, db: Session):
-    new_entry = Entry(user_id=user_id, competition_id=competition_id)
+@access_token_required
+def create_entry(request:Request, entry:EntryCreateSchema, db: Session):
+    user = db.query(User).filter(User.id == entry.user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    competition = (
+        db.query(Competition).filter(Competition.id == entry.competition_id).first()
+    ) 
+    if competition is None:
+        raise HTTPException(status_code=404, detail="competition not found")
+    
+    new_entry = Entry(user_id=user.id, competition_id=competition.id)
     db.add(new_entry)
     db.commit()
     db.refresh(new_entry)
     return new_entry
 
+@access_token_required
+def get_entry_by_id(request:Request, db: Session, entry_id: int):
+    entry = db.query(Entry).filter(Entry.id == entry_id).first()
+    if entry is None:
+        raise HTTPException(status_code=404, detail="entry not found")
+    return entry
 
-def get_entry_by_id(db: Session, entry_id: int):
-    return db.query(Entry).filter(Entry.id == entry_id).first()
 
+def update_entry(request:Request, entry_id: int, entry: EntryUpdateSchema, db: Session):
+    access_token = request.headers.get("Authorization")
+    payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+    user = db.query(User).filter(User.id == entry.user_id, User.auth_id == payload.get('id')).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    competition = (
+        db.query(Competition).filter(Competition.id == entry.competition_id).first()
+    )
+    if competition is None:
+        raise HTTPException(status_code=404, detail="competition not found")
 
-def update_entry(entry_id: int, user_id: int, competition_id: int, db: Session):
-    update_entry = get_entry_by_id(db, entry_id)
+    update_entry = get_entry_by_id(request, db, entry_id)
     update_entry.id = entry_id
-    update_entry.competition_id = competition_id
-    update_entry.user_id = user_id
+    update_entry.competition_id = competition.id
+    update_entry.user_id = user.id
     db.add(update_entry)
     db.commit()
     db.refresh(update_entry)
     return update_entry
 
 
-def delete_entry(entry_id: int, db: Session):
-    entry = get_entry_by_id(db, entry_id)
-    if entry:
-        db.delete(entry)
-        db.commit()
-        return "entry deleted"
-    else:
-        return "Entry not found"
+def delete_entry(request:Request, entry_id: int, db: Session):
+    entry = get_entry_by_id(request, db, entry_id)
+    db.delete(entry)
+    db.commit()
+    return {"message": "entry deleted"}
+    
 
-
-def delete_all_entry(db: Session):
-    all = db.query(Entry).delete()
+@access_token_required
+def delete_all_entry(request:Request, db: Session):
+    db.query(Entry).delete()
     db.commit()
